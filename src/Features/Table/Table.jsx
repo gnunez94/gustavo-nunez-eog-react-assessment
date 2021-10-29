6cf95104faec6541d66a017dbfd6c5d99ab16482
GusNA18 <gustavon@ritani.com>
2021-10-28 23:38:54 -0500

    create table with timeseries integration

--- src/Features/Table/Table.jsx
+++ src/Features/Table/Table.jsx
@@ -1,53 +1,32 @@
 import React from 'react';
-// import {
-//   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
-// } from 'recharts';
-
-// const thirtyMinuteSliding = window(duration('30m'));
-// const thirtyMinutes = duration({ minutes: 30 });
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
+} from 'recharts';
 
 const Table = ({ metrics, measurements }) => {
-  metrics.forEach(metric => {
-    // const timeRange = measurements[metric].timerange([
-    //   new Date() - 30 * 60 * 1000, new Date().getTime(),
-    // ]).toJSON().timerange;
+  const data = metrics.map(metric => {
+    const range = measurements[metric].filter(e => e.toJSON().time > new Date() - 30 * 60 * 1000);
 
-    console.log('range', measurements[metric].filter(e => e.toJSON().time > new Date() - 30 * 60 * 1000).toJSON());
+    const rangeJson = range.toJSON();
 
-    const measurementRange = measurements[metric].slice(new Date() - 30 * 60 * 1000);
+    const formattedData = rangeJson.points.map(measurement => ({
+      time: measurement[0], value: measurement[1], unit: measurement[2],
+    }));
 
-    console.log(metric, measurementRange.count());
+    return { name: rangeJson.name, data: formattedData };
   });
 
-  // metrics.map((metric) => data.push(measurements[metric].fixedWindowRollup({
-  //   window: thirtyMinuteSliding,
-  // })));
-
-  // console.log(data);
   return (
-    // <ResponsiveContainer width="100%" height="100%">
-    //   <LineChart
-    //     width={500}
-    //     height={300}
-    //     data={data}
-    //     margin={{
-    //       top: 5,
-    //       right: 30,
-    //       left: 20,
-    //       bottom: 5,
-    //     }}
-    //   >
-    //     <CartesianGrid strokeDasharray="3 3" />
-    //     <XAxis dataKey="name" />
-    //     <YAxis yAxisId="left" />
-    //     <YAxis yAxisId="right" orientation="right" />
-    //     <Tooltip />
-    //     <Legend />
-    //     <Line yAxisId="left" type="monotone" dataKey="value" stroke="#8884d8" />
-    //     {/* <Line yAxisId="right" type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
-    //   </LineChart>
-    // </ResponsiveContainer>
-    <></>
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={500} height={300}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="time" type="category" />
+        <YAxis dataKey="value" />
+        <Tooltip />
+        <Legend />
+        {data.map((s) => (<Line dataKey="value" data={s.data} name={s.name} key={s.name} />))}
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
